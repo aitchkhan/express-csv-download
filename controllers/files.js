37991edfc8987da8957079f7ccd4749d50a774a4
aitchkhan <aitchkhan@gmail.com>
2018-07-28 14:23:37 +0500

    fix typo in comments

--- controllers/files.js
+++ controllers/files.js
@@ -1,6 +1,7 @@
 // package use to transform json to csv string
 const stringify = require('csv-stringify');
 const posts = require('../posts.json');
+
 module.exports = {
   downloadCsv
 };
@@ -9,13 +10,14 @@ function downloadCsv(req, res) {
   // adding appropriate headers, so browsers can start downloading
   // file as soon as this request starts to get served
   res.setHeader('Content-Type', 'text/csv');
-  res.setHeader('Content-Disposition', 'attachment; filename=\"' + 'download' + '.csv\"');
+  res.setHeader('Content-Disposition', 'attachment; filename=\"' + 'download-' + Date.now() + '.csv\"');
   res.setHeader('Cache-Control', 'no-cache');
   res.setHeader('Pragma', 'no-cache');
 
   // ta-da! this is cool, right?
-  // stringify gives you a writeable stream, that can be directly piped to "res"
-  // since res is an abstraction over http's response object which supports "streams"
+  // stringify return a readable stream, that can be directly piped
+  // to a writeable stream which is "res" (the response object from express.js)
+  // since res is an abstraction over node http's response object which supports "streams"
   stringify(posts, { header: true })
     .pipe(res);
 };
